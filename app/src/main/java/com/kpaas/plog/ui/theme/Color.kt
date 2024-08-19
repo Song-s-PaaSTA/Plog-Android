79977214acdc507e3da5158ed5a79b846ecf8ebd
gaeulzzang <gaeulzzang@gmail.com>
2024-08-20 01:20:48 +0900

    #2 [SETTING] color system 구성

--- app/src/main/java/com/kpaas/plog/ui/theme/Color.kt
+++ app/src/main/java/com/kpaas/plog/ui/theme/Color.kt
@@ -2,10 +2,29 @@ package com.kpaas.plog.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Gray
+val Gray50 = Color(0xFFF1F1F1)
+val Gray100 = Color(0x4DC4C4C4)
+val Gray150 = Color(0xFFD9D9D9)
+val Gray200 = Color(0xFFC7CDD2)
+val Gray250 = Color(0xFFB3B3B3)
+val Gray300 = Color(0xFFA2AAB2)
+val Gray350 = Color(0xFF979797)
+val Gray400 = Color(0xFF949494)
+val Gray450 = Color(0xFF777777)
+val Gray500 = Color(0xFF374957)
+val Gray550 = Color(0xFF292D32)
+val Gray600 = Color(0xFF000000)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Green
+val Green50 = Color(0xFFA3E494)
+val Green100 = Color(0xFF1DC800)
+val Green150 = Color(0xFF02BC7D)
+val Green200 = Color(0xFF3AB67D)
+val Green250 = Color(0xFF1C4520)
+
+// Yellow
+val Yellow50 = Color(0xFFFCEFB4)
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
