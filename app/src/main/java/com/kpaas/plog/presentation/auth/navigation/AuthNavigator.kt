34127571b408cf2fd32bd915e0731c77218dd12c
gaeulzzang <gaeulzzang@gmail.com>
2024-08-31 18:33:29 +0900

    #28 [REFACTOR] back stack 수정

--- app/src/main/java/com/kpaas/plog/presentation/auth/navigation/AuthNavigator.kt
+++ app/src/main/java/com/kpaas/plog/presentation/auth/navigation/AuthNavigator.kt
@@ -5,36 +5,21 @@ import androidx.navigation.NavHostController
 class AuthNavigator(
     val navController: NavHostController
 ) {
-    fun navigateLogin() {
-        navController.navigate("login") {
+    fun navigateSignup() {
+        navController.navigate("signup")
+    }
+
+    fun navigateBoarding() {
+        navController.navigate("boarding") {
             popUpTo("login") {
                 inclusive = true
             }
         }
     }
-    fun navigateBack() {
-        navController.popBackStack()
-    }
 
-    fun navigateSignup() {
-        navController.navigate("signup") {
-            popUpTo(navController.graph.startDestinationId) {
-                inclusive = true
-            }
-            launchSingleTop = true
-        }
-    }
-
-    fun navigateBoarding() {
-        navController.navigate("boarding") {
-            popUpTo(navController.graph.startDestinationId) {
-                inclusive = true
-            }
-        }
-    }
     fun navigateMain() {
-        navController.navigate("main"){
-            popUpTo(0) {
+        navController.navigate("main") {
+            popUpTo("boarding") {
                 inclusive = true
             }
             launchSingleTop = true
