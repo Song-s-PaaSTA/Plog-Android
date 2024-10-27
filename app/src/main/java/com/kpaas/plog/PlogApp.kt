31b6df0d5e5ee9140f665ad832706d28e7428ebd
gaeulzzang <gaeulzzang@gmail.com>
2024-10-27 20:15:11 +0900

    proguard rule 설정

--- app/src/main/java/com/kpaas/plog/PlogApp.kt
+++ app/src/main/java/com/kpaas/plog/PlogApp.kt
@@ -16,10 +16,27 @@ class PlogApp : Application() {
     override fun onCreate() {
         super.onCreate()
         setTimber()
+        initNaverMapSdk()
+        initKakaoSdk()
+        initNaverIdLoginSdk()
+    }
+
+    private fun initKakaoSdk() {
+        KakaoSdk.init(this, KAKAO_API_KEY)
+    }
+
+    private fun initNaverIdLoginSdk() {
+        NaverIdLoginSDK.initialize(
+            this,
+            NAVER_CLIENT_ID,
+            NAVER_CLIENT_SECRET,
+            getString(R.string.app_name)
+        )
+    }
+
+    private fun initNaverMapSdk() {
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NaverCloudPlatformClient(NAVER_MAP_CLIENT_ID)
-        KakaoSdk.init(this, KAKAO_API_KEY)
-        NaverIdLoginSDK.initialize(this, NAVER_CLIENT_ID, NAVER_CLIENT_SECRET, getString(R.string.app_name))
     }
 
     private fun setTimber() {
